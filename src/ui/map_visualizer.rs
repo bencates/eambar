46552ba21c9061f32db891f4d64acb74b287c757
Ben Cates <ben.cates@gmail.com>
2022-07-25 13:59:59 -0600

    Hex map

--- src/ui/map_visualizer.rs
+++ src/ui/map_visualizer.rs
@@ -1,16 +1,38 @@
 use crate::prelude::*;
 
 pub fn visualize_map(ctx: &mut BTerm, map: &Map) {
+    ctx.cls();
+    ctx.set_active_console(1);
+    ctx.cls();
+
     let map_dimensions = map.dimensions();
+
+    let rotation = Radians::new(0.0);
+    let scale = PointF::new(1.0, 1.0);
 
     for x in 0..map_dimensions.x {
         for y in 0..map_dimensions.y {
-            let pos = Point::new(x, y);
+            let map_pos = Point::new(x, y);
 
-            if let Ok(appearance) = Appearance::try_from(&map[pos]) {
-                ctx.set(
-                    x,
-                    y,
+            if let Ok(appearance) = Appearance::try_from(&map[map_pos]) {
+                let mut pos = PointF::new(x as f32, y as f32);
+
+                if x % 2 == 0 {
+                    pos.y += 0.5;
+                }
+
+                log::trace!(
+                    "Printing '{}' to {:?}, color {:?}",
+                    appearance.glyph,
+                    pos,
+                    appearance.color
+                );
+
+                ctx.set_fancy(
+                    pos,
+                    0,
+                    rotation,
+                    scale,
                     appearance.color.fg,
                     appearance.color.bg,
                     to_cp437(appearance.glyph),
@@ -18,4 +40,6 @@ pub fn visualize_map(ctx: &mut BTerm, map: &Map) {
             }
         }
     }
+
+    ctx.set_active_console(0);
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,8 +6,8 @@ use crate::prelude::*;
 pub use main_menu::main_menu;
 pub use map_visualizer::visualize_map;
 
-pub const TERM_WIDTH: i32 = 92;
-pub const TERM_HEIGHT: i32 = 60;
+pub const TERM_WIDTH: i32 = 80;
+pub const TERM_HEIGHT: i32 = 50;
 
 #[derive(Component)]
 pub struct Appearance {
@@ -18,7 +18,9 @@ pub struct Appearance {
 pub fn setup() -> BResult<BTerm> {
     log::debug!("Generating a {TERM_WIDTH}x{TERM_HEIGHT} console");
 
-    BTermBuilder::simple(TERM_WIDTH, TERM_HEIGHT)?
+    BTermBuilder::simple80x50()
+        .with_fancy_console(80, 50, "terminal8x8.png")
         .with_title("Roguelike Tutorial")
+        .with_vsync(false)
         .build()
 }
